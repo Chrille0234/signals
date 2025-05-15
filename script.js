08cbac3a97cf4d0498a6aca6b98ee563c6be9540
Christian Madsen <113505641+Chrille0234@users.noreply.github.com>
2025-05-15 15:52:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-import {Signal} from "./signal.js";
+import { signal } from "./signal.js";
 
-let counter = new Signal(0, "counter");
+const counter = signal(0, "counter")
 
-const greeting = new Signal("Hello world!", "greeting")
+const button = document.querySelector("button")
 
-function increment(){
-    counter.value += 1
-}
-
-function updateGreeting(event) {
-    const value = event.target.value
-    greeting.value = value
-}
-
-document.querySelector("button")?.addEventListener("click", increment)
-document.querySelector("input")?.addEventListener("input", updateGreeting)
+button.addEventListener("click", () => counter.update(prev => ++prev))
